278f55fff59e0ecd8a6d4a87ed9e4d878d7f756b
Elyse Iradukunda <ellycreativity8@gmail.com>
2025-04-04 17:53:52 +0200

    hi

---
+++ Irael.com/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Header from './assets/Header'
+import Body from './Body'
+
+function App() {
+  
+  return (
+    <>
+    
+
+<Header/>
+  <br />
+<Body/>
+
+    </>
+  )
+}
+
+export default App
