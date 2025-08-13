2be6409a08d02fe0c3cf0ffd3e1b29e00a86059c
Irael <ellycreativity8@gmail.com>
2025-08-13 11:13:59 +0200

    Irael Profile staffus

---
+++ IraelProfile/src/components/images.jsx
@@ -0,0 +1,13 @@
+function Image(){
+ 
+    <>
+    <div className="flex gap-4 justify-center flex-wrap">
+      <img src="/img1.jpg" alt="pic1" className="w-48 h-auto" />
+      <img src="/img2.jpg" alt="pic2" className="w-48 h-auto" />
+      <img src="/img3.jpg" alt="pic3" className="w-48 h-auto" />
+    </div>
+    </>
+
+
+}
+export default Image;
---
+++ IraelProfile/src/components/menu.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import { IoMdHome } from "react-icons/io";
+import { GiSelfLove } from "react-icons/gi";
+import { FaPersonChalkboard } from "react-icons/fa6";
+import { IoMdSettings } from "react-icons/io";
+import { TiSocialAtCircular } from "react-icons/ti";
+function Menu(){
+
+    return(
+    <>
+    <nav className="">
+        <div className=" flex ml-10 mt-4">
+            <ul className="flex space-x-10">
+                <li className="flex gap-2"><IoMdHome className="text-2xl"/><a href="">Home</a></li>
+                <div className="flex space-x-10 ml-150" >
+                    <li className="flex gap-2"><FaPersonChalkboard className="text-2xl"/><a href="">About Me</a></li>
+                    <li className="flex gap-2"><GiSelfLove className="text-2xl"/><a href="">Hobbies</a></li>
+                    <li className="flex gap-2"><IoMdSettings className="text-2xl"/><a href="">Work </a></li>
+                    <li className="flex gap-2"><TiSocialAtCircular className="text-2xl"/><a href="">Contact Me </a></li>
+                </div>
+            </ul>
+        </div>
+    </nav>
+
+    </>
+    )
+
+
+}
+export default Menu;
