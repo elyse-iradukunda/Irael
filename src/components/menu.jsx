8e6d913d16f6ca76f9a9ac4649bd4cb44584d3f8
Irael <ellycreativity8@gmail.com>
2025-08-14 08:19:53 +0200

    changes to my site overthere!!

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,30 +1,42 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { IoMdHome } from "react-icons/io";
 import { GiSelfLove } from "react-icons/gi";
 import { FaPersonChalkboard } from "react-icons/fa6";
 import { IoMdSettings } from "react-icons/io";
 import { TiSocialAtCircular } from "react-icons/ti";
-function Menu(){
 
-    return(
-    <>
-    <nav className="">
-        <div className=" flex  mt-4">
-            <ul className="flex space-x-10">
-                <li className="flex gap-2"><IoMdHome className="text-2xl"/><a href="">Home</a></li>
-                <div className="flex space-x-10 ml-20" >
-                    <li className="flex gap-2"><FaPersonChalkboard className="text-2xl"/><a href="">About Me</a></li>
-                    <li className="flex gap-2"><GiSelfLove className="text-2xl"/><a href="">Hobbies</a></li>
-                    <li className="flex gap-2"><IoMdSettings className="text-2xl"/><a href="">Work </a></li>
-                    <li className="flex gap-2"><TiSocialAtCircular className="text-2xl"/><a href="">Contact Me </a></li>
-                </div>
-            </ul>
-        </div>
+function Menu() {
+  return (
+    <nav className="w-full bg-white shadow-md">
+      <div className="flex justify-center mt-4">
+        <ul className="flex flex-col sm:flex-row sm:items-center sm:space-x-10">
+        
+          <li className="flex items-center gap-2 p-2 hover:text-blue-500 transition-colors">
+            <IoMdHome className="text-2xl" />
+            <a href="#">Home</a>
+          </li>
+          <div className="flex flex-col sm:flex-row sm:space-x-10 sm:ml-20">
+            <li className="flex items-center gap-2 p-2 hover:text-blue-500 transition-colors">
+              <FaPersonChalkboard className="text-2xl" />
+              <a href="#">About Me</a>
+            </li>
+            <li className="flex items-center gap-2 p-2 hover:text-blue-500 transition-colors">
+              <GiSelfLove className="text-2xl" />
+              <a href="#">Hobbies</a>
+            </li>
+            <li className="flex items-center gap-2 p-2 hover:text-blue-500 transition-colors">
+              <IoMdSettings className="text-2xl" />
+              <a href="#">Work</a>
+            </li>
+            <li className="flex items-center gap-2 p-2 hover:text-blue-500 transition-colors">
+              <TiSocialAtCircular className="text-2xl" />
+              <a href="#">Contact Me</a>
+            </li>
+          </div>
+        </ul>
+      </div>
     </nav>
-
-    </>
-    )
-
-
+  );
 }
+
 export default Menu;
